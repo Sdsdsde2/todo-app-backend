fb0af6c708aa34ce09adfa123fc087cd731cbe8b
Sdsdsde2 <imspammails31@gmail.com>
2021-07-04 11:21:33 -0500

    added bcrypt and jwt

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_07_04_150726) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task_title"
+    t.string "task_info"
+    t.integer "task_priority"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "password_digest"
+    t.integer "task_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_users_on_task_id"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Task.destroy_all
+
+User.create(first_name: "shane", last_name: "rachal", user_name: "sdsdsde2", password_digest: "shane123")
+Task.create(task_title: "Finish Todo App", task_info: "This is a 'test-task' for the todo app.", task_priority: 3)
